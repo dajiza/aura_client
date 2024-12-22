8d38e4c473b682f5710bd8c8b37a79e3a8419f83
sa.chen <sa.chen@flatfeecorp.com>
2024-12-22 16:26:41 +0800

    vercel

--- src/routers/index.js
+++ src/routers/index.js
@@ -15,7 +15,7 @@ NProgress.configure({
 });
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     strict: true,
 });
