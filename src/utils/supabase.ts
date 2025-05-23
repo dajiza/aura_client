37665e7460acd4f4867a7fd7a9ae5f3a42b55dd0
sa.chen <sa.chen@flatfeecorp.com>
2025-05-23 19:06:42 +0800

    feat: intake

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,6 @@
 import { getSupabaseClient } from '@/lib/supabase';
 
 import { createClient as createPropelAuthClient } from '@propelauth/javascript';
-console.log('🚀 ~ import.meta.env:', import.meta.env);
-
 export const authClient = createPropelAuthClient({
     // The base URL where your authentication pages are hosted. You can find this under the Frontend Integration section for your project.
     authUrl: import.meta.env.VITE_APP_PROPELAUTH_URL,
